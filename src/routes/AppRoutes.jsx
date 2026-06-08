a909fb68929d3560d9de7a5dfcda91e428a9a434
emmauopeople <mbimunyui_uopeople@yahoo.com>
2026-06-08 18:41:10 -0500

    Add app routes

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PageShell from "../components/layout/PageShell";
+import HomePage from "../pages/HomePage";
+import ProjectsPage from "../pages/ProjectsPage";
+import ProjectDetailPage from "../pages/ProjectDetailPage";
+import ResumePage from "../pages/ResumePage";
+import NotFoundPage from "../pages/NotFoundPage";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <PageShell>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/projects/:slug" element={<ProjectDetailPage />} />
+          <Route path="/resume" element={<ResumePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </PageShell>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
