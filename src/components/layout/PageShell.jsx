252149f15cd0949418cb19b6a9de2173e0ea5720
emmauopeople <mbimunyui_uopeople@yahoo.com>
2026-06-08 17:38:52 -0500

    Add page shell layout

---
+++ src/components/layout/PageShell.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+function PageShell({ children }) {
+  return (
+    <div className="min-h-screen bg-slate-950 text-slate-100">
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+}
+
+export default PageShell;
