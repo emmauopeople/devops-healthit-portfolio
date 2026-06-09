a2e2dca8554dceaf7de01d7ebdcb04c89bbd3498
emmauopeople <mbimunyui_uopeople@yahoo.com>
2026-06-09 17:46:05 -0500

    Redirect projects page to first case study

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,25 +1,8 @@
-import ProjectCard from "../components/projects/ProjectCard";
-import Container from "../components/common/Container";
-import SectionHeader from "../components/common/SectionHeader";
+import { Navigate } from "react-router-dom";
 import { projects } from "../data/projects";
 
 function ProjectsPage() {
-  return (
-    <section className="py-20">
-      <Container>
-        <SectionHeader
-          eyebrow="Projects"
-          title="Case studies organized by real-world engineering problems"
-          description="Each project is structured to explain the problem, solution, architecture, implementation, deployment, monitoring, troubleshooting, results, and lessons learned."
-        />
-        <div className="mt-10 grid gap-6 md:grid-cols-2 xl:grid-cols-3">
-          {projects.map((project) => (
-            <ProjectCard key={project.slug} project={project} />
-          ))}
-        </div>
-      </Container>
-    </section>
-  );
+  return <Navigate to={`/projects/${projects[0].slug}`} replace />;
 }
 
 export default ProjectsPage;
