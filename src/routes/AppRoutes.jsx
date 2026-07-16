bc6114519ef0131ea19b61af421ba947d90384a3
emmauopeople <mbimunyui_uopeople@yahoo.com>
2026-07-16 17:15:21 -0500

    Add route for local Kubernetes church project

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import PageShell from "../components/layout/PageShell";
 import HomePage from "../pages/HomePage";
 import ProjectsPage from "../pages/ProjectsPage";
 import ProjectDetailPage from "../pages/ProjectDetailPage";
+import LocalKubernetesChurchPage from "../pages/LocalKubernetesChurchPage";
 import ResumePage from "../pages/ResumePage";
 import NotFoundPage from "../pages/NotFoundPage";
 
@@ -13,6 +14,7 @@ function AppRoutes() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/projects/local-kubernetes-church-microservices" element={<LocalKubernetesChurchPage />} />
           <Route path="/projects/:slug" element={<ProjectDetailPage />} />
           <Route path="/resume" element={<ResumePage />} />
           <Route path="*" element={<NotFoundPage />} />
